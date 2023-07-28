518d123ad26d494c35b2294a8d8da4bee64d1272
jskaria18 <jynsskaria3@gmail.com>
2023-07-29 02:05:12 +0530

    First Changes by Architects

---
+++ src/main/java/Cucumber/APIAutomation/NewTestArchitects.java
@@ -0,0 +1,10 @@
+package Cucumber.APIAutomation;
+
+public class NewTestArchitects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/Cucumber/APIAutomation/TestUpdate.java
@@ -0,0 +1,10 @@
+package Cucumber.APIAutomation;
+
+public class TestUpdate {
+
+	public static void main(String[] args) {
+		System.out.println("Just to see new updates");
+		System.out.println("Just to see new updates from Architects");
+	}
+
+}
